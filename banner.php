760e65339d562ce0793d4ae12699435fbd519a00
cnoid <10182309+cnoid@users.noreply.github.com>
2023-08-24 19:04:09 +0200

    Create banner.php

---
+++ banner.php
@@ -0,0 +1,12 @@
+<div class="gdpr-cookie-consent">
+    <div class="cookie-text">
+        We use cookies to ensure the best experience and analyze our traffic. By clicking "Accept", you consent to our use of cookies.
+    </div>
+    <div class="cookie-buttons">
+        <button type="button" class="accept-cookies">Accept</button>
+        <button type="button" class="dismiss-cookies">Dismiss</button>
+    </div>
+    <div>
+        <a href="/privacy-policy" class="policies">Privacy Policy</a> | <a href="/terms" class="policies">Terms and Conditions</a>
+    </div>
+</div>
